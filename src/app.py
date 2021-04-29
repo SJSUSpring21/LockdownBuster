8c8e0108c2b7eae1cd6fbc6a76c52b600bc7b516
Rakesh Nagarajappa <78629375+rakeshn1@users.noreply.github.com>
2021-04-29 16:17:38 -0700

    Backend changes

--- src/app.py
+++ src/app.py
@@ -1,28 +1,111 @@
-from flask import Flask
+from flask import Flask, render_template, request,json
+import requests
 import pandas as pd
 from fbprophet import Prophet
 app = Flask(__name__)
 
-county = pd.read_csv("../dataset/CountyWiseCases.csv")
-county['date'] = pd.DatetimeIndex(county['date'])
-import datetime
-df = county.loc[(county["area"] == "Santa Clara")]
-df = df[df['date'] > '2020-12-01']
-df = df[["date","reported_cases"]]
-df['ds'] = pd.DatetimeIndex(df['date'])
-df.drop('date', axis=1, inplace=True)
-df_train = df[0:]
-df_train.columns = ['y','ds']
-m = Prophet(interval_width=0.95,changepoint_prior_scale=0.8)
-model = m.fit(df_train)
-future = m.make_future_dataframe(periods=90,freq='D')
-forecast = m.predict(future)
+def getPrediction(county_name):
+    county = pd.read_csv("../dataset/CountyWiseCases.csv")
+    county['date'] = pd.DatetimeIndex(county['date'])
+    df = county.loc[(county["area"] == county_name)]
+
+    #getting current status
+    curr = get_current_status(df)
+
+    #getting active case prediction
+    apr = get_active_case_prediction(df)
+
+    #getting vaccination prediction
+    vpr = get_vaccination_prediction(county_name)
+    return {"prediction_ac" : apr[['ds','yhat']].iloc[0,1],"prediction_vc" : vpr[['ds','yhat']].iloc[0,1],"current_status":curr}
+
+def get_vaccination_prediction(county_name):
+    df = pd.read_csv("../dataset/covid19vaccinesbycounty.csv", usecols=[0, 1, 13])
+    df['administered_date'] = pd.DatetimeIndex(df['administered_date'])
+    # Getting the county wise data
+    df = df.loc[(df["county"] == county_name)]
+    del df['county']
+    df = df.iloc[::-1]
+    df['ds'] = pd.DatetimeIndex(df['administered_date'])
+    df.drop('administered_date', axis=1, inplace=True)
+    df_train = df[0:]
+    df_train.columns = ['y', 'ds']
+    return vac_predMod(df_train)
+
+
+
+def get_active_case_prediction(df):
+    df = df[df['date'] > '2020-12-01']
+    df = df[["date", "reported_cases"]]
+    df['ds'] = pd.DatetimeIndex(df['date'])
+    df.drop('date', axis=1, inplace=True)
+    df_train = df[0:]
+    df_train.columns = ['y', 'ds']
+    pr = ac_preMod(df_train)
+    return pr
+
+
+def vac_predMod(df_train):
+    m = Prophet(interval_width=0.95)
+    model = m.fit(df_train)
+    future = m.make_future_dataframe(periods=180, freq='D')
+    forecast = m.predict(future)
+    return forecast
+
+def ac_preMod(df_train):
+    m = Prophet(interval_width=0.95, changepoint_prior_scale=0.5)
+    model = m.fit(df_train)
+    future = m.make_future_dataframe(periods=90, freq='D')
+    forecast = m.predict(future)
+    return forecast
+
+def get_current_status(df):
+    cs = df[df['date'] > '2021-04-16']
+    cs = cs[1:]
+
+    # calculating current number of cases per 100k
+    mean = cs.agg({'reported_cases': ['mean']})
+    population = cs.population.unique()
+    div = population[0] / 100000
+    curr_cases = mean['reported_cases'] / div
+    curr_cases = curr_cases['mean']
+    print(curr_cases)
+
+    # calculating average positive tests
+    c_sum = 0;
+    for index, row in cs.iterrows():
+        t_avg = (row['positive_tests'] / row['reported_tests']) * 100
+        c_sum += t_avg
+    pos = c_sum / 7;
+    print(pos)
+
+    if curr_cases < 2.0 and pos <2.0:
+        status = 'minimal'
+    elif (curr_cases > 2.0 and  curr_cases < 5.9) and (pos <4.9):
+        status = 'moderate'
+    elif (curr_cases > 6.0 and  curr_cases < 10) and (pos <8):
+        status = 'substantial'
+    else:
+        status = 'widespread'
+    return status
+
+
+
+
     
 @app.route('/')
 def hello_world():
-    return {"pediction":forecast[['ds','yhat']].iloc[0,1]}
+    return "Predictor"	
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    Fuel_Type_Diesel=0
+    if request.method == 'POST':
+        county = json.loads(request.data)['county']
+        return getPrediction(county)
 
 
 if __name__ == '__main__':
     app.run()
+
 
